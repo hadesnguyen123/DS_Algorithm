263768e6792cc44631a0a349b575ba5b6bf3281f
Nguyễn Hoàng <96653565+hadesnguyen123@users.noreply.github.com>
2024-11-30 23:03:03 +0700

    Priority queue

---
+++ src/Tree/PriorityQueue.js
@@ -0,0 +1,31 @@
+//Priorityqueue là một cấu trúc dữ liệu, mỗi phần tử có một mức độ ưu tiên 
+//Thay vì xử lý FIFO thì PQ xử lý phần tử có mức độ ưu tiên cao nhât
+//Cài bđặt bằng Binary Heap (heap kiểu binary)
+
+class QElement{
+    constructor(element, priority){
+        this.element = element;
+        this.priority = priority;
+    }
+}
+
+//PriorityQueue class
+
+class PriorityQueue {
+    //An array is used to implement priority
+    constructor(){
+        this.items = []
+    }
+
+    //function to be implemented
+    enqueue(item, priority){        //add element
+        //create object from queue element
+            
+        //
+    }
+    dequeue()
+    front()
+    isEmpty()
+    printPQueue()
+
+}
